8a7a7d89d49e8ebcbcf81abfae5bf766a4ce3b10
Adam Novak <adnosk@gmail.com>
2022-12-01 15:57:01 +0100

    Write pseudocode

--- js/game.js
+++ js/game.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+// function getComputerChoice where computer selects randomly from ‘Rock’, ‘Paper’ or ‘Scissors’
+// prompt player to enter his selection
+// function playRound which determines the winner of the current round
+// make it case insensitive 
+// return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+
+
+// function game in which the function playRound will loop for 5 rounds and display winner/looser
