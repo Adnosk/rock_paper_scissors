5f5261d140bbe91fdb7c1441bf78143822db081d
Adam Novak <adnosk@gmail.com>
2022-12-01 17:46:08 +0100

    Fix playRound logic

--- js/game.js
+++ js/game.js
@@ -3,7 +3,7 @@ const getPlayerChoice = prompt('Select Rock, Paper, Scissors');
 
 // function getComputerChoice where computer selects randomly from ‘Rock’, ‘Paper’ or ‘Scissors’
 function getComputerChoice() {
-    const choice = ['Rock', 'Paper', 'Scissor'];
+    const choice = ['Rock', 'Paper', 'Scissors'];
     return choice[Math.floor(Math.random() * choice.length)];
 }
 
@@ -12,29 +12,21 @@ function getComputerChoice() {
 // return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'Rock' && computerSelection === 'Rock') {
+    if (playerSelection === computerSelection) {
         console.log('It is a tie!');
-    } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+    } else if (playerSelection === 'Rock' && computerSelection === 'Paper' || 
+      playerSelection === 'Scissors' && computerSelection === 'Rock' ||
+      playerSelection === 'Paper' && computerSelection === 'Scissors') {
         console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-    } else if(playerSelection === 'Paper' && computerSelection === 'Paper') {
-        console.log('It is a tie!');
-    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
-        console.log(`You lose! ${computerSelection} beat ${playerSelection}`);
-    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-    }  else if(playerSelection === 'Scissors' && computerSelection === 'Paper') {
-        console.log('It is a tie!');
-    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors' ||
+      playerSelection === 'Paper' && computerSelection === 'Rock' ||
+      playerSelection === 'Scissors' && computerSelection === 'Paper') {
         console.log(`You win! ${playerSelection} beats ${computerSelection}`);
     } else console.log('something went wrong');
 }
    
  
   const computerSelection = getComputerChoice();
-  console.log(playRound(getPlayerChoice, getComputerChoice()));
+  console.log(playRound(getPlayerChoice, computerSelection));
 
 // function game in which the function playRound will loop for 5 rounds and display winner/looser
