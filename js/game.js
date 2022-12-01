725b4c7fe6d2f6f947e87e56b66dafbf3da5eb4d
Adam Novak <adnosk@gmail.com>
2022-12-01 16:27:13 +0100

    Add prompt user selection

--- js/game.js
+++ js/game.js
@@ -5,8 +5,9 @@ const choice = ['Rock', 'Paper', 'Scissor'];
 function getComputerChoice() {
     return choice[Math.floor(Math.random() * choice.length)];
 }
-console.log(getComputerChoice());
+
 // prompt player to enter his selection
+
 // function playRound which determines the winner of the current round
 // make it case insensitive 
 // return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
@@ -15,7 +16,7 @@ function playRound(playerSelection, computerSelection) {
     // your code here!
   }
    
-  const playerSelection = "rock";
+  const playerSelection = prompt('Select Rock, Paper, Scissor');
   const computerSelection = getComputerChoice();
   console.log(playRound(playerSelection, computerSelection));
 
