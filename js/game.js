1b9473e48fc8084b3733a622dd1e71526174bec3
Adam Novak <adnosk@gmail.com>
2022-12-02 10:06:04 +0100

    Add final score logic and reset counter after game

--- js/game.js
+++ js/game.js
@@ -1,3 +1,6 @@
+// starting score
+let computerScore = 0;
+let playerScore = 0;
 
 
 // function getComputerChoice where computer selects randomly from ‘Rock’, ‘Paper’ or ‘Scissors’
@@ -16,19 +19,43 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection === 'rock' && computerSelection === 'paper' || 
       playerSelection === 'scissors' && computerSelection === 'rock' ||
       playerSelection === 'paper' && computerSelection === 'scissors') {
+        computerScore++;
         return `You lose! ${computerSelection} beats ${playerSelection}`;
     } else if (playerSelection === 'rock' && computerSelection === 'scissors' ||
       playerSelection === 'paper' && computerSelection === 'rock' ||
       playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore++;
         return `You win! ${playerSelection} beats ${computerSelection}`;
     } else console.log('something went wrong');
   }   
  
+function game() {  
   
   for (let i = 0; i < 5; i++) {
-    // prompt player to enter his selection
-    const getPlayerChoice = prompt('Select Rock, Paper, Scissors').toLowerCase();
-    const computerSelection = getComputerChoice();
+      
+      // prompt player to enter his selection
+      const getPlayerChoice = prompt('Select Rock, Paper, Scissors').toLowerCase();
+      const computerSelection = getComputerChoice();
 
-    console.log(playRound(getPlayerChoice, computerSelection));
+      console.log(playRound(getPlayerChoice, computerSelection)) === "You win"
   }
+  
+  if (playerScore > computerScore) {
+    console.log(`You win the game with the final score of ${playerScore}:${computerScore}`); 
+    // reset score
+    computerScore = 0;
+    playerScore = 0;
+  } else if (playerScore === computerScore) {
+    console.log(`It is a tie with the final score of ${playerScore}:${computerScore}`);
+    // reset score
+    computerScore = 0;
+    playerScore = 0;
+  } else {
+    console.log(`You lose the game with the final score of ${playerScore}:${computerScore}`);
+    // reset score
+    computerScore = 0;
+    playerScore = 0;
+  }
+}
+
+game();
